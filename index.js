aa60b9c4beeccbd959df71bb6b41e3e61cc836a7
ryanagungsamudra <ryansamudra67@gmail.com>
2023-03-03 09:12:52 +0700

    fix: deploy bugs

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) => {
   });
 });
 
-const port = 5000;
-app.listen(process.env.PORT || port, (req, res) => {
+const port = process.env.PORT || 5000;
+app.listen(port, (req, res) => {
   console.log(`Server successfully running on port ${port}`);
 });
